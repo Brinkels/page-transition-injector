5c707ccbc8711b2595eeecd815b1b0357c7a2174
Ivo Carbajo <ivo@brinkels.com>
2020-12-03 15:03:52 +0100

    Added arrival animation

--- js/page-transition-injector.js
+++ js/page-transition-injector.js
@@ -1,6 +1,8 @@
 var PTI = {
     options: {
-        generateSheetsPerElement: true
+        generateSheetsPerElement: true,
+        transitionTime: 1000,
+        direction: 'right'
     },
     init: (options) => {
         if (typeof options != 'undefined') PTI.options = {...PTI.options, ...options};
@@ -32,30 +34,26 @@ var PTI = {
                     });
                 })
 
-                console.log(generatorSheets);
-
-                var builtSheets = PTI.generateSheets(generatorSheets, element.getAttribute('data-pti-transition-time'));
-
-                builtSheets.forEach(sheet => {
-                    document.getElementById('page-transition-container').appendChild(sheet);
-                });
+                PTI.sheetsLeaving(generatorSheets, element);
         
                 setTimeout(() => {
                     window.location.href = location;
-                }, PTI.options.transitionTime ? PTI.options.transitionTime : element.getAttribute('data-pti-transition-time') || 1000);
+                }, element.getAttribute('data-pti-transition-time') || PTI.options.transitionTime);
             });
         });
     },
-    generateSheets: (elementSheets, transitionTime) => {
+    generateSheets: (elementSheets, transitionTime, inverted) => {
 
         //Generate transition sheets
         var builtSheets = [];
         var sheetCounter = 0;
         var sheetAmount = PTI.options.generateSheetsPerElement ? elementSheets.length : PTI.options.sheets.length;
-        var totalTransitionTime = PTI.options.transitionTime ? PTI.options.transitionTime : transitionTime || 1000;
+        var totalTransitionTime = transitionTime || PTI.options.transitionTime;
         var timePerSheet = totalTransitionTime / sheetAmount;
 
         var sheets = PTI.options.generateSheetsPerElement ? elementSheets : PTI.options.sheets;
+
+        if (inverted) sheets = sheets.slice().reverse();
 
         sheets.forEach(sheet => {
             sheetCounter++;
@@ -63,6 +61,7 @@ var PTI = {
             sheetDiv.style.backgroundColor = sheet.color;
             sheetDiv.style.width = '100vw';
             sheetDiv.style.height = '100vh';
+            if (!inverted) sheetDiv.style.zIndex = sheetCounter-1 + 10;
             
             switch (PTI.options.direction) {
                 case 'up':
@@ -79,22 +78,44 @@ var PTI = {
                 
                 case 'right':
                     sheetDiv.style.animationName = "scroll-right";
-                    break;
-
-                case 'down':
-                    
+                    break;                    
             
                 default:
                     sheetDiv.style.animationName = "scroll-up";
                     break;
             }
 
+            if (inverted) {
+                sheetDiv.style.animationDirection = 'reverse';
+                sheetDiv.style.zIndex = sheets.length + 20 - sheetCounter;
+                sheetDiv.style.animationDelay = timePerSheet * sheets.length - sheetCounter;
+            }
+            sheetDiv.style.animationFillMode = "forwards";
             sheetDiv.style.animationDuration = timePerSheet * sheetCounter + 'ms';
-            sheetDiv.style.animationDelay = timePerSheet * sheetCounter-1;
             sheetDiv.classList.add('page-transition-sheet');
+            console.log(timePerSheet);
             builtSheets.push(sheetDiv);
         });
 
+        setTimeout(() => {
+            document.querySelectorAll('.page-transition-sheet').forEach(element => {
+                element.remove();
+            })
+        }, totalTransitionTime+100);
+
         return builtSheets;
+    },
+    sheetsLeaving: (sheets, element) => {
+        var builtSheets = PTI.generateSheets(sheets, typeof element !== 'undefined' ? element.getAttribute('data-pti-transition-time'): PTI.options.transitionTime);
+        builtSheets.forEach(sheet => {
+            document.getElementById('page-transition-container').appendChild(sheet);
+        });
+    },
+    sheetsArriving: sheets => {
+        var builtSheets = PTI.generateSheets(sheets, PTI.options.transitionTime, true);
+        builtSheets.forEach(sheet => {
+            document.getElementById('page-transition-container').appendChild(sheet);
+        });
     }
+
 }
